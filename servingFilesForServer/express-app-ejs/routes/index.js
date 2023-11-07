621b0d453feec4f5fe103f88b5c5b7669e8deaf7
Kuldeep Singh <kuldeepsingh1828@gmail.com>
2023-11-07 12:20:52 +0530

    reading get and post
    we learnt about get and post router in express

--- servingFilesForServer/express-app-ejs/routes/index.js
+++ servingFilesForServer/express-app-ejs/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var userData =[];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', userData });
+});
+
+/* GET home page. */
+router.get('/login', function(req, res, next) {
+  const data = req.query;
+  console.log(data)
+  res.render('loginForm');
+});
+
+/* GET home page. */
+router.post('/login', function(req, res, next) {
+  const data = req.body;
+  userData =[...userData, data]
+  console.log(data)
+  res.render('loginForm');
 });
 
 module.exports = router;
